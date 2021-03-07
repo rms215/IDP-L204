de174832b84769cfecf997e359b3af6cfd4bbc8a
tjc74 <79928050+tjc74@users.noreply.github.com>
2021-03-07 00:05:35 +0000

    Add files via upload
    Thomas' version

--- controllers/IDP_controller/IDP_controller.cpp
+++ controllers/IDP_controller/IDP_controller.cpp
@@ -1,28 +1,26 @@
 // File:          IDP_controller.cpp
 #include <webots/Robot.hpp>
-
-// Added a new include file
+#include <webots/GPS.hpp>
 #include <webots/Motor.hpp>
-#include <webots/LightSensor.hpp>
 #include <webots/DistanceSensor.hpp>
+#include <webots/Compass.hpp>
 
 #define TIME_STEP 64
 #define MAX_SPEED 10
 
-// All the webots classes are defined in the "webots" namespace
 using namespace webots;
-
 Robot *robot = new Robot();
 
-// get the time step of the current world.
-int timeStep = (int)robot->getBasicTimeStep();
 
-DistanceSensor *ds[3];
-char dsNames[3][40] = {"ds_right","ds_left","Sharp's IR sensor GP2Y0A02YK0F"};
-
-/*LightSensor *light_sensor = robot->getLightSensor("TEPT4400");
-light_sensor->enable(TIME_STEP);
-*/
+//class GPS : public Device {
+/*class GPS : public Device {
+  virtual void enable(int samplingPeriod);
+  virtual void disable();
+  int getSamplingPeriod() const;
+  const double *getValues() const;
+  const double getSpeed() const;
+  // ...
+ };*/
 
 void move_forwards() {
    // get the motor devices
@@ -33,9 +31,9 @@ void move_forwards() {
    rightMotor->setPosition(INFINITY);
 
    // set up the motor speeds at 50% of the MAX_SPEED.
-   leftMotor->setVelocity(0.5 * MAX_SPEED);
-   rightMotor->setVelocity(0.5 * MAX_SPEED);
-}
+   leftMotor->setVelocity(1.0 * MAX_SPEED);
+   rightMotor->setVelocity(1.0 * MAX_SPEED);
+};
 void move_backwards() {
    // get the motor devices
    Motor *leftMotor = robot->getMotor("Wheel_L");
@@ -45,8 +43,8 @@ void move_backwards() {
    rightMotor->setPosition(INFINITY);
 
    // set up the motor speeds at 50% of the MAX_SPEED.
-   leftMotor->setVelocity(-0.5 * MAX_SPEED);
-   rightMotor->setVelocity(-0.5 * MAX_SPEED);
+   leftMotor->setVelocity(-1.0 * MAX_SPEED);
+   rightMotor->setVelocity(-1.0 * MAX_SPEED);
 }
 void rotate_CW() {
    // get the motor devices
@@ -89,31 +87,42 @@ void close_arms() {
    rightMotor->setPosition(0.0);
 }
 
-//Reads distance sensors
-void scanOnSpot(){
-    double dsValues[3];
-    for (int i = 0; i < 3 ; i++)
-    dsValues[i] = ds[i]->getValue();
-    rotate_ACW();
-}
-
 int main(int argc, char **argv) {
-  //Initialising US and IR sensors
-//Initialise array of pointers of type DistanceSensor device tag
+ // wb_robot_init();
+  GPS *gps = robot->getGPS("gps");
+  gps->enable(TIME_STEP);
+  //start_pos_green = []   //reccord initial position
   
-//Retrieving device tags and enabling with refresh time step
-  for(int i = 0; i < 3; i++){
-    ds[i] = robot->getDistanceSensor(dsNames[i]);
-    ds[i]->enable(TIME_STEP); //TIME_STEP defines rate at which sensor is refreshed
-  }
-
- while (robot->step(TIME_STEP) != -1){
-    
-    // Main (algorithmic loop)
+  Compass *compass = robot->getCompass("compass");
+  compass->enable(TIME_STEP);
+  // write code to reccord initial orientation
+  double initial_position[3] = {0.0, 1.0, 2.0};
+  int i = 0;
+  if (robot->step(TIME_STEP) == 64){
+    initial_position[0] = gps->getValues()[0];
+    initial_position[1] = gps->getValues()[1];
+    initial_position[2] = gps->getValues()[2];
+   //const double initial_north = compass->getValues();
+  };
+  while (robot->step(TIME_STEP) != -1){
+    if (i == 0){ //recccording initial values
+      initial_position[0] = gps->getValues()[0];
+      initial_position[1] = gps->getValues()[1];
+      initial_position[2] = gps->getValues()[2];
+      //const double initial_north = compass->getValues();
+      };
+    const double *position = gps->getValues();
+    const double *north = compass->getValues();
     move_forwards();
-    
- };
-    
+
+    std::cout <<  position[0] << ", "<< position[1] << ", "<< position[2] << std::endl;
+    //std::cout <<  north[0] << ", "<< north[1] << ", "<< north[2] << std::endl;
+    std::cout <<  initial_position[0] << ", "<< initial_position[1] << ", "<< initial_position[2] << std::endl;
+    std::cout << i << std::endl;
+    i += 1;
+    }
+
  delete robot;
+
  return 0;
- }
+}
