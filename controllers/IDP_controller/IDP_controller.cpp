7bc6c5a2f4fb8f57840aaca2f131531e723d3d07
tjc74 <79928050+tjc74@users.noreply.github.com>
2021-03-07 18:35:22 +0000

    Add files via upload
    added bearing fuction

--- controllers/IDP_controller/IDP_controller.cpp
+++ controllers/IDP_controller/IDP_controller.cpp
@@ -3,13 +3,8 @@
 #include <webots/GPS.hpp>
 #include <webots/Motor.hpp>
 #include <webots/DistanceSensor.hpp>
-<<<<<<< HEAD
-#include <webots/GPS.hpp>
-#include <webots/Compass.hpp>
-=======
 #include <webots/Compass.hpp>
 #include <webots/LightSensor.hpp>
->>>>>>> bba3d423cdfd2278b6d4657ac49438e97e80f9e0
 
 #define TIME_STEP 64
 #define MAX_SPEED 10
@@ -26,23 +21,10 @@ char dsNames[4][40] = {"us_right","us_left","ir_left","ir_right"};
 light_sensor->enable(TIME_STEP);
 */
 
-//Initialising GPS
-GPS *gps = robot->getGPS("gps");
-gps->enable(TIME_STEP);
-
-//Initialising compass
-Compass *compass = robot->getCompass("compass");
-compass->enable(TIME_STEP);
-
-// get the motor devices
-Motor *leftMotor = robot->getMotor("Wheel_L");
-Motor *rightMotor = robot->getMotor("Wheel_R");
-
-// get the arm motor devices
-Motor *leftArmMotor = robot->getMotor("Arm_L");
-Motor *rightArmMotor = robot->getMotor("Arm_R");
-
 void move_forwards() {
+   // get the motor devices
+   Motor *leftMotor = robot->getMotor("Wheel_L");
+   Motor *rightMotor = robot->getMotor("Wheel_R");
    // set the target position of the motors
    leftMotor->setPosition(INFINITY);
    rightMotor->setPosition(INFINITY);
@@ -52,6 +34,9 @@ void move_forwards() {
    rightMotor->setVelocity(0.5 * MAX_SPEED);
 }
 void move_backwards() {
+   // get the motor devices
+   Motor *leftMotor = robot->getMotor("Wheel_L");
+   Motor *rightMotor = robot->getMotor("Wheel_R");
    // set the target position of the motors
    leftMotor->setPosition(INFINITY);
    rightMotor->setPosition(INFINITY);
@@ -61,15 +46,7 @@ void move_backwards() {
    rightMotor->setVelocity(-0.5 * MAX_SPEED);
 }
 
-<<<<<<< HEAD
-void move_position(double x){
-   // set the target position of the motors
-   leftMotor->setPosition(x);
-   rightMotor->setPosition(x);
-}
-
-=======
-/*float move_backwards_position(x) {
+/*double move_backwards_position(x) {
 // get the motor devices
    Motor *leftMotor = robot->getMotor("Wheel_L");
    Motor *rightMotor = robot->getMotor("Wheel_R");
@@ -81,8 +58,10 @@ void move_position(double x){
    leftMotor->setVelocity(-0.5 * MAX_SPEED);
    rightMotor->setVelocity(-0.5 * MAX_SPEED);
 }*/
->>>>>>> bba3d423cdfd2278b6d4657ac49438e97e80f9e0
 void rotate_CW() {
+   // get the motor devices
+   Motor *leftMotor = robot->getMotor("Wheel_L");
+   Motor *rightMotor = robot->getMotor("Wheel_R");
    // set the target position of the motors
    leftMotor->setPosition(INFINITY);
    rightMotor->setPosition(INFINITY);
@@ -92,6 +71,9 @@ void rotate_CW() {
    rightMotor->setVelocity(-0.5 * MAX_SPEED);
 }
 void rotate_ACW() {
+   // get the motor devices
+   Motor *leftMotor = robot->getMotor("Wheel_L");
+   Motor *rightMotor = robot->getMotor("Wheel_R");
    // set the target position of the motors
    leftMotor->setPosition(INFINITY);
    rightMotor->setPosition(INFINITY);
@@ -101,22 +83,38 @@ void rotate_ACW() {
    rightMotor->setVelocity(0.5*MAX_SPEED);
 }
 void open_arms() {
+   // get the motor devices
+   Motor *leftMotor = robot->getMotor("Arm_L");
+   Motor *rightMotor = robot->getMotor("Arm_R");
    // set the target position of the motors
-   leftArmMotor->setPosition(0.5);
-   rightArmMotor->setPosition(-0.5);
+   leftMotor->setPosition(0.5);
+   rightMotor->setPosition(-0.5);
 }
 void close_arms() {
+   // get the motor devices
+   Motor *leftMotor = robot->getMotor("Arm_L");
+   Motor *rightMotor = robot->getMotor("Arm_R");
    // set the target position of the motors
    leftMotor->setPosition(0.0);
    rightMotor->setPosition(0.0);
 }
 
-<<<<<<< HEAD
-void scanOnSpot(){
-=======
+double get_bearing_in_degrees(const double *north) {
+  //const double *north = wb_compass_get_values(tag);
+  double rad = atan2(north[0], north[2]);
+  double bearing = (rad - 1.5708) / M_PI * 180.0;
+  if (bearing < 0.0)
+    bearing = bearing + 360.0;
+  return bearing;
+}
+
+void rotate_360() { //function to be used in scan_on_spot
+}
+  
+
+
 //Reads distance sensors
-/*void scanOnSpot(){
->>>>>>> bba3d423cdfd2278b6d4657ac49438e97e80f9e0
+/*void scan_on_spot(){
     for (int i = 0; i < 4 ; i++){
     double dsValues[4];
     dsValues[i] = ds[i]->getValue();
@@ -126,17 +124,6 @@ void scanOnSpot(){
 
 int main(int argc, char **argv) {
 
-<<<<<<< HEAD
-    // write code to reccord initial orientation
-    double initial_position[3];
-    int i = 0;
-    if(i == 0){
-      initial_position[0] = gps->getValues()[0];
-      initial_position[1] = gps->getValues()[1];
-      initial_position[2] = gps->getValues()[2];
-     //const double initial_north = compass->getValues();
-    };
-=======
   //Initialising GPS
   GPS *gps = robot->getGPS("gps");
   gps->enable(TIME_STEP);
@@ -144,34 +131,42 @@ int main(int argc, char **argv) {
   
   Compass *compass = robot->getCompass("compass");
   compass->enable(TIME_STEP);
-  // write code to reccord initial orientation
-  double initial_position[3] = {0.0, 1.0, 2.0};
-  int i = 0;
-  if (i == 0){
+  // write code to record initial orientation
+  
+  // position and orientation pointers
+  const double *position = gps->getValues();
+  const double *north = compass->getValues();
+
+//Retrieving device tags and enabling with refresh time step
+  for(int i = 0; i < 4; i++){
+    ds[i] = robot->getDistanceSensor(dsNames[i]);
+    ds[i]->enable(TIME_STEP); //TIME_STEP defines rate at which sensor is refreshed
+  }
+  
+  
+  double initial_position[3] = {0.0, 1.0, 2.0}; //initiate initial position
+  int i = 0; //initiate time step counter
+ while (robot->step(TIME_STEP) != -1){
+
+    if (i == 0){
     initial_position[0] = gps->getValues()[0];
     initial_position[1] = gps->getValues()[1];
     initial_position[2] = gps->getValues()[2];
    //const double initial_north = compass->getValues();
   };
->>>>>>> bba3d423cdfd2278b6d4657ac49438e97e80f9e0
   
-//Retrieving device tags and enabling with refresh time step
-    for(int i = 0; i < 4; i++){
-      ds[i] = robot->getDistanceSensor(dsNames[i]);
-      ds[i]->enable(TIME_STEP); //TIME_STEP defines rate at which sensor is refreshed
-    }
-   
-  
-   while (robot->step(TIME_STEP) != -1){
-     
-      move_position(-4000);
-      
-      // Main (algorithmic loop)
-      scanOnSpot();
-      
-   }
-      
-   delete robot;
-   return 0;
- 
-}
+    // Main (algorithmic loop)
+    rotate_ACW();
+    double bearing = get_bearing_in_degrees(north);
+    std::cout << bearing << std::endl;
+    //std::cout <<  position[0] << ", "<< position[1] << ", "<< position[2] << std::endl;
+    //std::cout <<  north[0] << ", "<< north[1] << ", "<< north[2] << std::endl;
+    //std::cout <<  initial_position[0] << ", "<< initial_position[1] << ", "<< initial_position[2] << std::endl;
+    //std::cout << i << std::endl;
+    
+    i+=1;
+ }
+    
+ delete robot;
+ return 0;
+ }
