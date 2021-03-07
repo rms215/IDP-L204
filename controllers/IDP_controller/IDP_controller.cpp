4092d7e8163a39fb87ea80ec92ce57518ea20baa
rageoveralostpenny <wu.cindyx@gmail.com>
2021-03-07 02:22:56 +0000

    Robot proto file updated

--- controllers/IDP_controller/IDP_controller.cpp
+++ controllers/IDP_controller/IDP_controller.cpp
@@ -14,11 +14,8 @@ using namespace webots;
 
 Robot *robot = new Robot();
 
-// get the time step of the current world.
-int timeStep = (int)robot->getBasicTimeStep();
-
-DistanceSensor *ds[3];
-char dsNames[3][40] = {"ds_right","ds_left","Sharp's IR sensor GP2Y0A02YK0F"};
+DistanceSensor *ds[4];
+char dsNames[4][40] = {"us_right","us_left","ir_left","ir_right"};
 
 /*LightSensor *light_sensor = robot->getLightSensor("TEPT4400");
 light_sensor->enable(TIME_STEP);
@@ -48,6 +45,19 @@ void move_backwards() {
    leftMotor->setVelocity(-0.5 * MAX_SPEED);
    rightMotor->setVelocity(-0.5 * MAX_SPEED);
 }
+
+void move_backwards_position(x)
+// get the motor devices
+   Motor *leftMotor = robot->getMotor("Wheel_L");
+   Motor *rightMotor = robot->getMotor("Wheel_R");
+   // set the target position of the motors
+   leftMotor->setPosition(x);
+   rightMotor->setPosition(x);
+
+   // set up the motor speeds at 50% of the MAX_SPEED.
+   leftMotor->setVelocity(-0.5 * MAX_SPEED);
+   rightMotor->setVelocity(-0.5 * MAX_SPEED);
+}
 void rotate_CW() {
    // get the motor devices
    Motor *leftMotor = robot->getMotor("Wheel_L");
@@ -69,8 +79,8 @@ void rotate_ACW() {
    rightMotor->setPosition(INFINITY);
 
    // set up the motor speeds at 50% of the MAX_SPEED.
-   leftMotor->setVelocity(-0.5 * MAX_SPEED);
-   rightMotor->setVelocity(0.5 * MAX_SPEED);
+   leftMotor->setVelocity(-0.5*MAX_SPEED);
+   rightMotor->setVelocity(0.5*MAX_SPEED);
 }
 void open_arms() {
    // get the motor devices
@@ -91,28 +101,46 @@ void close_arms() {
 
 //Reads distance sensors
 void scanOnSpot(){
-    double dsValues[3];
-    for (int i = 0; i < 3 ; i++)
+    for (int i = 0; i < 4 ; i++){
+    double dsValues[4];
     dsValues[i] = ds[i]->getValue();
     rotate_ACW();
+    }
 }
 
 int main(int argc, char **argv) {
-  //Initialising US and IR sensors
-//Initialise array of pointers of type DistanceSensor device tag
+
+  //Initialising GPS
+  GPS *gps = robot->getGPS("gps");
+  gps->enable(TIME_STEP);
+  //start_pos_green = []   //reccord initial position
+  
+  Compass *compass = robot->getCompass("compass");
+  compass->enable(TIME_STEP);
+  // write code to reccord initial orientation
+  double initial_position[3] = {0.0, 1.0, 2.0};
+  int i = 0;
+  if (i = 0){
+    initial_position[0] = gps->getValues()[0];
+    initial_position[1] = gps->getValues()[1];
+    initial_position[2] = gps->getValues()[2];
+   //const double initial_north = compass->getValues();
+  };
   
 //Retrieving device tags and enabling with refresh time step
-  for(int i = 0; i < 3; i++){
+  for(int i = 0; i < 4; i++){
     ds[i] = robot->getDistanceSensor(dsNames[i]);
     ds[i]->enable(TIME_STEP); //TIME_STEP defines rate at which sensor is refreshed
   }
+  
+  
 
  while (robot->step(TIME_STEP) != -1){
     
     // Main (algorithmic loop)
-    move_forwards();
+    rotate_ACW();
     
- };
+ }
     
  delete robot;
  return 0;
